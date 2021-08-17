117d0a0ed79e711f3deb4d2086214251d27d31e0
Watcharin <twatcharin90@gmail.com>
2021-08-17 15:34:51 +0700

    39list-keys

--- client2/src/components/product/ProductItem.js
+++ client2/src/components/product/ProductItem.js
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 class ProductItem extends Component {
   constructor(props) {
     super(props);
-    console.log("constructor|" + props.productName);
+    // console.log("constructor|" + props.productName);
   }
   render() {
-    const { productName, unitPrice } = this.props;
+    const { productName, unitPrice, thumbnail } = this.props;
     return (
-      <div>
-        <p>{productName}</p>
-        <p>{unitPrice}</p>
+      <div className="col-md-3 col-sm-6">
+        <img className="img-fluid img-thumbnail" src={thumbnail} />
+        <h5 className="mt-2">{productName}</h5>
+        <p className="text-right title">{unitPrice} THB</p>
+        <button className="btn- btn-block btn-secondary title">ซื้อ</button>
+        <hr/>
       </div>
     );
   }
--- client2/src/components/product/ProductList.js
+++ client2/src/components/product/ProductList.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import ProductItem from './ProductItem'
 
 export class ProductList extends Component {
+
+
+    showProducts(){
+        return this.props.products&&this.props.products.map(product =>(
+            // <ProductItem productName={product.productName} unitPrice={product.unitPrice} />
+            <ProductItem key={product.productId} {...product} /> //พอเป็น list ต้องใส่ key  ที่ unique
+        ))
+    }
+
     render() {
         return (
-            <div>
-                ProductList
+            <div className= 'row'>
+                {this.showProducts()}
+
+                
+                
             </div>
         )
     }
